9025deb474b8af93615c50c2553a06b81bee2f69
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-02-03 18:50:19 +0530

    added commits and changes

---
+++ Lecture_008_Switch_Cases_and_functions/008_01_Switch_Cases.js
@@ -0,0 +1,96 @@
+let num = 2 
+
+switch (num){
+   case 1: 
+      console.log('First')
+      break;
+
+   case 2:
+       console.log('second')
+    break;
+
+    case 3:
+        console.log('thee')
+     break;
+
+     default:
+        console.log('Its a default case')
+ 
+}
+
+// Output:
+// second
+
+
+
+
+//------------------------------------------------------------
+
+
+
+// check for charectors 
+
+let num = 'A'
+
+switch (num){
+   case 1: 
+      console.log('First')
+      break;
+
+   case 'A':
+       console.log('second')
+    break;
+
+    case 3:
+        console.log('thee')
+     break;
+
+     default:
+        console.log('Its a default case')
+ 
+}
+
+
+// Output:
+// second
+
+
+//--------------------------------------------------------------
+
+
+
+// nested switch statements
+
+
+
+let num = 'A'
+
+switch (num){
+   case 1: 
+      console.log('First')
+      break;
+
+   case 'A': switch (num){
+       case 'A': console.log(`value of num is: ${num}`)
+       break;
+   }
+    break;
+
+    case 3:
+        console.log('thee')
+     break;
+
+     default:
+        console.log('Its a default case')
+ 
+}
+
+// Output:
+// value of num is: A
+
+
+// -----------------------------------------------------------
+
+
+
+
