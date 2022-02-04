7208f87493f302c864b0d9a07930f63c8135ecf1
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-02-04 12:01:20 +0530

    edit the file add new problem

--- Lecture_008_Switch_Cases_and_functions/008_Unresolved.js
+++ Lecture_008_Switch_Cases_and_functions/008_Unresolved.js
@@ -1,1 +1,20 @@
 // find the number of 10, 20 , 50  , 100 rupee notes in 1242 rupees. 
+
+
+// finding the power of a number  using the forloops 
+
+
+
+let a = 2
+let b = 4
+
+let ans = 1
+
+for (let i = 1; i <=b; i++){
+    ans = ans * a
+}
+
+console.log(`the answer is: ${ans}`)
+
+// Output:
+// the answer is: 16
