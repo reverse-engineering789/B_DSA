867b9a8fca9a6de699175668a425b7c1c9fda144
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-02-05 14:48:10 +0530

    created a new file accessing the value code

---
+++ 009_Arrays/009_01_02_Accessing_the_values.js
@@ -0,0 +1,11 @@
+// acceccing the values of an array: 
+
+let second = [1,2,3,4,5]
+
+console.log('value at index 0 :' + second[0])
+console.log('value at index 4 :' + second[4])
+
+
+// Output:
+// value at index 0: 1
+// value at index 4: 5
