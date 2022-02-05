0d66a30b20fcaafbd271f574a01d62f66a1c6eeb
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-02-05 14:45:29 +0530

    created new file for declaring the array

---
+++ 009_Arrays/009_01_01_declaring_The_Array.js
@@ -0,0 +1,11 @@
+// declaring the array: 
+
+const number = [15]
+
+console.log('value at index 0:' + number[0])
+console.log('value at index 15:' + number[15])
+
+
+// Output:
+// value at index 0:15
+// value at index 15:undefined
