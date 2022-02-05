37a2c9b2e8c082f823b2332deb204611a0907c32
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-02-05 23:22:14 +0530

    created file for the code to find minimum and maximum number in an array

---
+++ 009_Arrays/009_04_Maximum_and_Minimum_Number_In_Array.js
@@ -0,0 +1,75 @@
+let numbers = [9,5,78,5,4,3343,6,8,7,56,1,5,6,86,6,]
+
+console.log(Math.max(...numbers))
+
+ // Output:
+ // 3343
+
+
+
+
+console.log(Math.min(...numbers))
+
+ // Output:
+ // 1
+
+
+
+
+// //-------------------------------------------------------------------
+
+
+console.log(Math.max.apply(Math, numbers))
+ // Output:
+ // 3343
+
+
+// //-------------------------------------------------------------------
+
+
+
+
+
+
+var getMin = (number) => {
+      
+    var min = number[0];
+
+    for (var i = 0; i <=number.length; i++){
+        if (number[i] < min){
+            min = number[i]
+        }
+    }
+    return min
+}
+
+
+var getMax = (number) => {
+      
+    var max = number[0];
+
+    for (var i = 0; i <=number.length; i++){
+        if (number[i] > max){
+            max = number[i]
+        }
+    }
+    return max
+}
+
+var number = [9,5,78,5,4,3343,6,8,7,56,1,5,6,86,6,]
+
+console.log('the maximum number in the given array is: ' + getMax(number))
+console.log('the minimum number in the given array is: ' + getMin(number))
+
+
+// Output:
+// the maximum number in the given array is: 3343
+// the minimum number in the given array is: 1
+
+
+
+// //-----------------------------------------------------------------------------
+
+
+
+
