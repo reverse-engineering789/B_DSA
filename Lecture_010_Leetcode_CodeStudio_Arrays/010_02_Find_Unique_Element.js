d5fae5417be3db4a33ce0cbbdda7509570dc61d8
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-02-06 18:39:14 +0530

    create file for code-find unique element

---
+++ Lecture_010_Leetcode_CodeStudio_Arrays/010_02_Find_Unique_Element.js
@@ -0,0 +1,22 @@
+
+
+let findUnique = (arr) => {
+    let ans = 0
+
+    for(let i = 0; i < arr.length; i++){
+        ans = ans^arr[i]
+    }
+    return ans
+}
+
+
+
+let test1 = [2,4,7,2,7]
+let test2 = [1,3,1,3,6,6,7,10,7]
+
+console.log('the unique element in test1 is: ' + findUnique(test1))
+console.log('the unique element in test2 is: ' + findUnique(test2))
+
+// Output:
+// the unique element in test1 is: 4
+// the unique element in test2 is: 10
