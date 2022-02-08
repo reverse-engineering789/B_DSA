7c9e6aa8d8ae5810e5581a8cadbbd82a284efa9c
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-02-08 23:13:20 +0530

    able to get output but getting 'undefined' at the end

--- Lecture_010_Leetcode_CodeStudio_Arrays/010_Unresolved_Problems.js
+++ Lecture_010_Leetcode_CodeStudio_Arrays/010_Unresolved_Problems.js
@@ -48,4 +48,118 @@ console.log(pairSum(arr))
 // }
 
 
+
+
+
+// // --------------------------------------------------------
+
+
+
+
+
+
+
+
+// not able to git proper output: every thing is OK. the only pronbelm is getting 
+// undefined extra at the end.
+
+let printArray = (arr) => {
+    for (let i = 0; i < arr.length; i++){
+        console.log(arr[i])
+    }
+    
+}
+
+let sortOne = (arr) => {
+     let left = 0
+     let right = arr.length-1
+
+     while (left < right){
+         while (arr[left] == 0 && left < right){
+             left++
+         }
+         while (arr[right] == 1 && left < right){
+             right--
+         }
+
+         if(left<right){
+             let temp
+             temp = arr[left] 
+            arr[left] = arr[right]
+             arr[right] = temp 
+         left++
+         right--
+         }
+     }
+}
+
+let arr1 = [1,1,0,0,0,0,1,0]
+
+sortOne(arr1)
+console.log(printArray(arr1))
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// void printArray(int arr[], int n) {
+//     for(int i=0; i<n; i++) {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+// void sortOne(int arr[], int n) {
+
+//     int left = 0, right = n-1;
+
+//     while(left < right) {
+
+//         while(arr[left] == 0 && left < right )  {
+//             left++;
+//         }
+
+//         while(arr[right]==1 && left < right){
+//             right--;
+//         }
+
+//         //agar yha pohoch gye ho, iska matlab
+//         //arr[left]==1 and arr[right]==0
+//         if(left<right)
+//         {
+//             swap(arr[left], arr[right]);
+//             left++;
+//             right--;
+//         }
+//     }
+
+// }
+
+// int main() {
+
+//     int arr[8] = {1,1,0,0,0,0,1,0};
+
+//     sortOne(arr, 8);
+//     printArray(arr, 8);
+
+
+//     return 0;
+// }
+
+
+
+// Output:
+// 0
+// 0
+// 0
+// 0
+// 0
+// 1
+// 1
+// 1
+// undefined
+
+
+
 // // --------------------------------------------------------
