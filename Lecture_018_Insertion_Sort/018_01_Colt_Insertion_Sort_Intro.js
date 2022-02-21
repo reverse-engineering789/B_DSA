adc6cd8bb3181b312359045ce117577bd27feb2d
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-02-21 20:47:06 +0530

    added insertion sort intro from dsa class

---
+++ Lecture_018_Insertion_Sort/018_01_Colt_Insertion_Sort_Intro.js
@@ -0,0 +1,27 @@
+// algorithm    Time Complexity    TimeComplexity    TimeComplexity     Space Complexity 
+//                   (Best)          (Average)          (Worst)             
+// Bubble Sort        O(n)            O(n)^2             O(n)^2              O(1)
+// Insertion Sort     O(n)            O(n)^2             O(n)^2              O(1)
+// Selection SOrt     O(n)^2          O(n)^2             O(n)^2              O(1)
+
+
+
+// Insertion Sort: 
+
+// Builds up the sort by gradually creating a larger left 
+// half which is always sorted
+
+//-------------------------------------------
+
+// Insertiom Sort Psuedocode 
+
+// * Start by picking the second element in the array 
+
+// * Now compare the second element with the one before it and swap 
+//   if necessary
+
+// * continue to the next element and if it is in the incorrect onder, 
+//   iterate through the sorted portion(i.e. the left side) to place 
+//   the element in the correct place. 
+
+//  * Repeat until the array is sorted
