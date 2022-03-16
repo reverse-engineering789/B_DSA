efb48885fba088b18dd1460b43f1294878d4ea1b
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-03-16 08:37:18 +0530

    22_08_02_L.B_LeetCode2_Remove_All_Occurences_of_substring - Not able to understand the code

---
+++ Lecture_022_All_about_Char_Arrays_Strings_and_Solving_LeetCode_Questions/022_Unresolved_5.js
@@ -0,0 +1,34 @@
+
+// 022_08_02_L.B_LeetCode2_Remove_All_Occurences_of_substring.
+
+// Not able to understand the code. 
+
+ var removeOccurrences = function(s, part) {
+    let result = []
+    const partLen = part.length
+    function endsWith() {
+        const resultLen = result.length
+        if (resultLen < partLen)
+            return false
+        
+        for (let offset = 1; offset <= partLen; offset++) {
+            const chPart = part[partLen - offset]
+            const chS = result[resultLen - offset]
+            if (chPart !== chS) return false
+        }
+        
+        return true
+    }
+    
+    
+    for (const ch of s) {
+        result.push(ch)
+        if (endsWith()) {
+            result.length -= partLen
+        }
+    }
+    
+    
+    result = result.join('')
+    return result
+}
