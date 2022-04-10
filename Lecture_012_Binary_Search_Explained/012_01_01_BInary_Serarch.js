cf7502d4a8832edeb8c35456e9f53829c53f88b9
abcpqr-123 <94470193+abcpqr-123@users.noreply.github.com>
2022-04-10 20:28:55 +0530

    modifed the code to get proper output

--- Lecture_012_Binary_Search_Explained/012_01_01_BInary_Serarch.js
+++ Lecture_012_Binary_Search_Explained/012_01_01_BInary_Serarch.js
@@ -1,3 +1,43 @@
+// new working code for all testcases
+
+let binarySearch = (arr, key) => {
+    let start = 0
+    let end = arr.length-1
+     
+    let  mid = Math.floor(( start + end ) / 2)
+
+    while (start <= end){
+        if (arr[mid] === key){
+            return mid 
+        }
+        if (key > arr[mid]){
+            start = mid + 1
+
+        }else{
+            end = mid - 1
+        }
+         mid =  (start + end) / 2
+         mid = Math.floor(mid)
+         
+        }
+        return -1
+        }
+              
+        let Even = [3,8,11,14,16,20]
+        let EvenIndex = binarySearch(Even,3)
+        console.log('index of 3 is: ' + EvenIndex)
+
+        let odd = [3,8,11,14,16]
+        let oddIndex = binarySearch(odd,3)
+        console.log('index of 3 is: ' + oddIndex)
+
+
+// Output:
+// index of 3 is: 0
+// index of 3 is: 0
+//------------------------------------------------------------
+
+// code before was returning -1
 // not able to get the proper output: 
 // binary search-accessing the index
 
