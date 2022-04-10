67f6ef4998af7780faa581bdf6fb05651871b297
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-10 11:57:11 +0530

    010_Unresolved_Problems.js

--- Lecture_010_Leetcode_CodeStudio_Arrays/010_Unresolved_Problems.js
+++ Lecture_010_Leetcode_CodeStudio_Arrays/010_Unresolved_Problems.js
@@ -1,3 +1,38 @@
+l
+// 010_03_Find_Duplicates
+// not giving proper output for test case: [2,4,3,6,6,3,4,2]
+
+
+
+let findDuplicate = (arr) => {
+    let ans = 0
+
+    for (let i = 0; i < arr.length; i++){
+        ans = ans^arr[i]
+    }
+
+    for (let i = 1; i < arr.length; i++){
+        ans = ans^i
+    }
+    return ans
+}
+
+let arr1 = [4,2,1,3,1] 
+let arr2 = [6,3,1,5,4,3,2]
+
+console.log('The duplicate element in arr1: ' + findDuplicate(arr1))
+console.log('The duplicate element in arr2: ' + findDuplicate(arr2))
+
+// Output:
+// The duplicate element in arr1: 1
+// The duplicate element in arr2: 3
+
+
+
+
+//-----------------------------------------------------------------------
+
+
 
 // 010_05_01_Pair_Sum.js
 
