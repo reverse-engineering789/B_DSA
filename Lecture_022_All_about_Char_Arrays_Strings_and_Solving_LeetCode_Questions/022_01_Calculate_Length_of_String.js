d68d31ebf84775c1ee4bb668912cc8bf91beafde
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-15 23:47:21 +0530

    022_01_Calculate_Length_of_String

--- Lecture_022_All_about_Char_Arrays_Strings_and_Solving_LeetCode_Questions/022_01_Calculate_Length_of_String.js
+++ Lecture_022_All_about_Char_Arrays_Strings_and_Solving_LeetCode_Questions/022_01_Calculate_Length_of_String.js
@@ -1,6 +1,18 @@
-let test = 'Surya Surya'
-let CalLength = test.length
-console.log(CalLength)
+// let test = 'SuryaSurya'
+// let CalLength = test.length
+// console.log(CalLength)
 
 // Output:
-// 11
+// 10
+
+
+let getLength = (arr) =>{
+  let count = 0
+    for (let i = 0; i < arr.length; i++){
+       count++
+   }
+    console.log(count)
+ }
+
+
+getLength('Surya')
