259ce0e1f7a1394205e73fba00e7eda5f7b67f1f
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-16 00:19:41 +0530

    020_01_02_L.B_Reverse_Array_Simple_Example

--- Lecture_020_LeetCode_Codestudio_Questions_Arrays/020_01_02_L.B_Reverse_Array_Simple_Example.js
+++ Lecture_020_LeetCode_Codestudio_Questions_Arrays/020_01_02_L.B_Reverse_Array_Simple_Example.js
@@ -1,8 +1,4 @@
-// Problem: Not able to get the output:
 
-// L. B solution: 
-
-// simple example FOR REVERSING: 
 
 
 let reverse = (v) => {
@@ -25,58 +21,14 @@ let print = (v) => {
     }
 }
 
-let v = [11,7,3,12,4]
+let v1 = [11,7,3,12,4]
+reverse(v1)
+print(v1)
 
+// Output:
+// 4 
+// 12 
+// 3  
+// 7  
+// 11
 
-
-//-------------------------------------------------------
-
-
-
-
-// c++ code: 
-
-// #include<iostream>
-// #include<vector>
-// using namespace std;
-
-// vector<int> reverse(vector<int> v) {
-
-//     int s = 0, e =v.size()-1;
-
-//     while(s<=e) {
-//         swap(v[s], v[e]);
-//         s++;
-//         e--;
-//     }
-
-//     return v;
-
-// }
-
-// void print(vector<int> v) {
-
-//     for(int i=0; i<v.size(); i++) {
-//         cout << v[i] << " ";
-//     }
-//     cout << endl;
-// }
-
-// int main() {
-
-//     vector<int> v ;
-
-//     v.push_back(11);
-//     v.push_back(7);
-//     v.push_back(3);
-//     v.push_back(12);
-//     v.push_back(4);
-
-//     vector<int> ans = reverse(v);
-
-//     cout << "Printing reverse Array" <<endl;
-//     print(ans);
-
-
-//     return 0;
-// }
