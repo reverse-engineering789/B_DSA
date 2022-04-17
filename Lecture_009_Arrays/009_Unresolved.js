6418c8b659792225f7c65c746a3ad0ad8a9abea5
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-18 04:46:02 +0530

    009_Unresolved

---
+++ Lecture_009_Arrays/009_Unresolved.js
@@ -0,0 +1,8 @@
+
+// // Unable to understand the code
+// console.log(Math.max.apply(Math, numbers))
+//  // Output:
+//  // 3343
+
+
+// // //-------------------------------------------------------------------
