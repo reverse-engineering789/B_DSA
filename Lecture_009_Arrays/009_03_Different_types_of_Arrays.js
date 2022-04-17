4bc08bc84654a975823d9d0ad2aed785ef65e163
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-18 04:42:51 +0530

    009_03_Different_types_of_Arrays.js

---
+++ Lecture_009_Arrays/009_03_Different_types_of_Arrays.js
@@ -0,0 +1,33 @@
+// charater type:
+
+
+// test = ['Surya', 'Sun', 'Moon', 'Star']
+
+// console.log(test[3])
+
+// // Output:
+// // Star
+
+// //------------------------------------------------------------
+
+
+printArray = (arr) => {
+    console.log('Printing the array: ')
+    for (let i = 0; i < arr.length; i++){
+         console.log(arr[i])
+    }
+    console.log('Printing Done.')
+}
+
+
+test = ['Surya', 'Sun', 'Moon', 'Star']
+printArray(test)
+
+
+// Output:
+// Printing the array: 
+// Surya
+// Sun
+// Moon
+// Star
+// Printing Done.
