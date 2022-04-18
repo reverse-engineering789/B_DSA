3cd931336d7133f31b1957b38090f6744c6f5c19
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-18 18:20:31 +0530

    005_03_Pre_Post_Increment-Decrement_Operator

---
+++ Lecture_005_Bitwise-Operators_For-Loops_Operator-precedence_Variable-Scoping/005_03_Pre_Post_Increment-Decrement_Operator.js
@@ -0,0 +1,29 @@
+// i = i+1 --> 
+// i++ -> Post Increment
+// ++i -> Pre Increment
+// i+=1 
+
+// i = i-1 --> 
+// i-- -> Post decrement 
+// --i -> Pre decrement
+// i-=1
+
+
+
+let i = 7
+
+console.log(++i)
+// 8
+console.log(i++)
+// 8, i = 9
+console.log(i--)
+// 9, i = 8
+console.log(--i)
+// 7, i = 7
+
+
+// Output:
+// 8
+// 8
+// 9
+// 7
