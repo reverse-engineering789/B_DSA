4c743229c0807ffcf17826ad1cf1294c1495ef06
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-20 17:40:32 +0530

    005_08_Use_Continue_ForLoop

---
+++ Lecture_005_Bitwise-Operators_For-Loops_Operator-precedence_Variable-Scoping/005_08_Use_Continue_ForLoop.js
@@ -0,0 +1,18 @@
+for (let i = 0; i < 5; i++){
+    console.log('Hi')
+    console.log('Hey')
+    continue
+    console.log('Why no reply? Whats the Problem? are  u angry on me? ')
+}
+
+// Output:
+// Hi
+// Hey
+// Hi
+// Hey
+// Hi
+// Hey
+// Hi
+// Hey
+// Hi
+// Hey
