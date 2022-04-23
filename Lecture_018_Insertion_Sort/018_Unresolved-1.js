c9877f1bcbad16eb65a2b54b138b284b4445430f
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-23 07:36:39 +0530

    modifiy Uresolved.js

--- Lecture_018_Insertion_Sort/018_Unresolved-1.js
+++ Lecture_018_Insertion_Sort/018_Unresolved-1.js
@@ -7,7 +7,7 @@ let insertionSort = (arr) => {
     for (let i = 1; i < arr.length; i++){
         let temp = arr[i]
         let j = i - 1
-        for (; j > 0; j--){
+        for (; j >= 0; j--){
             if (arr[j] > temp){
                 arr[j+1] = arr[j]
             }else{
