d2f53c879afcb79d6a761a5707d919b4b1e412e9
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-17 20:30:45 +0530

    004_04_Alpha_Pattern-2.js

---
+++ Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-2.js
@@ -0,0 +1,47 @@
+let n = 5;
+let string = "";
+// External loop
+for (let i = 1; i <= n; i++) {
+  // printing characters
+  for (let j = 0; j < i; j++) {
+    string += String.fromCharCode((i - 1) + 65);
+  }
+  string += "\n";
+}
+console.log(string);
+
+// Output:
+// A
+// B B
+// C C C
+// D D D D
+// E E E E E
+
+
+
+//------------------------
+
+
+
+// // My Code: 
+
+// My Code: 
+
+// const n = 5 
+
+// let string = ''
+
+// for (let i = 1; i <= n; i++){
+//     for (let j = 1; j <= i ; j++){
+//         string = string + String.fromCharCode((i) + 64)
+//     }
+//     string = string + '\n'
+// }
+// console.log(string)
+
+// // Output:
+// // A
+// // BB
+// // CCC
+// // DDDD
+// // EEEEE
