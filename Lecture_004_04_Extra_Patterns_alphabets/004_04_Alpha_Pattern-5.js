a98e9f68dd6be05d13dd15d9b99f24404b21efe1
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-17 20:32:25 +0530

    004_04_Alpha_Pattern-5.js

---
+++ Lecture_004_04_Extra_Patterns_alphabets/004_04_Alpha_Pattern-5.js
@@ -0,0 +1,34 @@
+let n = 5; // you can take input using prompt or change the value
+let string = "";
+// External loop
+for (let i = 1; i <= n; i++) {
+  for (let j = 0; j < n - i + 1; j++) {
+    string += String.fromCharCode((n - 1 - j) + 65);
+  }
+  string += "\n";
+}
+console.log(string);
+// Output:
+// EDCBA
+// EDCB
+// EDC
+// ED
+// E
+
+// -----------------------------------
+// const n = 5 
+// string = '';
+// for (let i = 1; i <= n; i++){
+//     for (let j = 1; j <= n-i+1; j++){
+//        string = string + String.fromCharCode((n+1-j)+64)
+//     }
+//     string = string + '\n'
+// }
+// console.log(string)
+
+// // Output:
+// // EDCBA
+// // EDCB
+// // EDC
+// // ED
+// // E
