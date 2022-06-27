540b356c8f017f8ed1505cf1d7bec1d13a69ce52
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-06-27 18:53:58 +0530

    Lecture_010_Leetcode_CodeStudio_Arrays/010_05_01_Pair_Sum.js

--- Lecture_010_Leetcode_CodeStudio_Arrays/010_05_01_Pair_Sum.js
+++ Lecture_010_Leetcode_CodeStudio_Arrays/010_05_01_Pair_Sum.js
@@ -1,31 +1,60 @@
 
+
+const pairSum = (arr, sum) => {
+    let   count = 0
+    let sumToatal = []
+         for (let i = 0; i <= arr.length-1; i++){
+             for (let j = i+1; j <= arr.length-1; j++){
+                 if (arr[i] + arr[j] === sum){
+                     sumToatal.push(arr[i], arr[j])
+                       count++ 
+                   console.log("(" + arr[i] + ", " + arr[j] + ")" );
+                 }
+             }
+         }
+         return count
+      }
+  
+      
+  
+  const number = 6
+  const array = [1,2,4,5,6,3]
+  console.log(pairSum(array, number))
+  
+  
+// // Output:
+// (1, 5)
+// (2, 4)
+// 2
+
+// // -------------------------------
 // L_B
 
-let pairSum = (arr, s) => {
-    let ans = []
+// let pairSum = (arr, s) => {
+//     let ans = []
 
-    for (let i = 0; i < arr.length; i++){
-        for (let j = i+1; j < arr.length; j++){
-            if (arr[i] + arr[j] == s){
+//     for (let i = 0; i < arr.length; i++){
+//         for (let j = i+1; j < arr.length; j++){
+//             if (arr[i] + arr[j] == s){
 
-                let temp = []
-                temp.push(min(arr[i], arr[j]))
-                temp.push(max(arr[i], arr[j]))
-                    ans.push(temp)
+//                 let temp = []
+//                 temp.push(min(arr[i], arr[j]))
+//                 temp.push(max(arr[i], arr[j]))
+//                     ans.push(temp)
 
-            }
-        }
-    }
-    arr.sort()
-   return ans
-}
-let s = 6
-let arr = [1,2,4,5,6,3]
-
-
-console.log(pairSum(arr))
+//             }
+//         }
+//     }
+//     arr.sort()
+//    return ans
+// }
+// let s = 6
+// let arr = [1,2,4,5,6,3]
 
 
+// console.log(pairSum(arr))
+
+//  ---------------C++-----------------
 // #include<vector>
 // using namespace std;
 // vector<vector<int> > pairSum(vector<int> &arr, int s){
@@ -49,3 +78,6 @@ console.log(pairSum(arr))
 
 
 //-------------------------------------------------------------
+
+
+
