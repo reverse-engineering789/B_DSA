f5f09e37406fe949dea4603c503b46cf115e3d5b
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-11-01 18:38:46 +0530

    025_01_Pointers.js

---
+++ Lecture_025_Pointers/025_01_Pointers.js
@@ -0,0 +1,16 @@
+// let num = 5;
+// console.log(num)
+
+// // Output:
+// // 5
+
+
+// // ---------------------------------------------------------
+
+
+// let num = 5;
+// console.log(num)
+
+// console.log(&num)
+
+
