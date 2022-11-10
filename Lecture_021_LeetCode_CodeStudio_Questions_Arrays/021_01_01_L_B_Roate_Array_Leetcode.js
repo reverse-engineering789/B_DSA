595984c8cbbe82fa6e47a6b7cb111e96d2bc6f12
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-11-11 00:53:54 +0530

    021_01_01_L_B_Roate_Array_Leetcode.js

--- Lecture_021_LeetCode_CodeStudio_Questions_Arrays/021_01_01_L_B_Roate_Array_Leetcode.js
+++ Lecture_021_LeetCode_CodeStudio_Questions_Arrays/021_01_01_L_B_Roate_Array_Leetcode.js
@@ -1,5 +1,21 @@
-
-
+// this solution works on vs code but not accepted 
+var rotate = function(nums, k) {
+    console.log(nums)
+    let empty1 = []
+    let empty2 = []
+   for (let i = k+1; i < nums.length; i++){
+       empty1.push(nums[i])
+      
+   }
+   for (let j = 0; j <= k; j++){
+       empty2.push(nums[j]) 
+       
+   }
+      return empty1.concat(empty2);
+};
+nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+console.log(rotate(nums, k))
 
 // Problem Qustion:
 
