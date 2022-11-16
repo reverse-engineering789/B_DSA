ba6bd69defab7db3ca7eb9e2f69c1602424ae929
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-11-16 09:43:51 +0530

    042_01_Class_and_Objects.cpp

---
+++ Lecture_042_OOPS_Concepts_in_C++/042_01_Class_and_Objects.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+class Hero {
+    // // Properties:
+    // char name[100];
+    // int health;
+    // char level;
+    
+
+    // Properties:
+    int health;
+};
+int main(){
+   
+   // Creation of object:
+   Hero h1;
+   cout << sizeof(h1) <<  endl;
+ return 0;
+}
+
+// Output:
+// integer takes 4 bytes:
+// 4
+
+
+
+
