22e1ddf8d8dcca737fe9ca3cfc320ef2a8a786c1
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-11-17 08:00:29 +0530

    042_08_02_This_KeyWord.cpp

---
+++ Lecture_042_OOPS_Concepts_in_C++/042_08_02_This_KeyWord.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class Hero {
+    // Properties
+    private:
+    int health;
+
+    public:
+    char level;
+    
+    Hero(){
+        cout << "Constructor Called: " << endl;
+    }
+
+    // Paramerterised Constructor
+    Hero(int health){
+        cout << "this -> : " << this << endl;
+        this -> health = health;
+    }
+
+    Hero(int health, char level){
+        this -> level = level;
+        this -> health = health;
+    }
+    void print(){
+        cout << level << endl;
+    }
+    int getHealth(){
+        return health;
+    }
+
+    char getLevel(){
+        return level;
+    }
+    void setHealth(int h){
+        health = h;
+    }
+
+    void setLevel(char ch){
+        level = ch;
+    }
+};
+
+int main(){
+  
+// object created statically
+Hero ramesh(10);
+// cout << "Adress of ramesh "  << &ramesh << endl;
+ramesh.print();
+
+//  dynamically
+Hero *h = new Hero(11);
+h -> print();
+
+Hero temp(22, 'B');
+temp.print();
+
+ return 0;
+}
+
+// Output:
+// this -> : 0x61fef4
+// ■
+// this -> : 0xf07f18
+// Output of this spcific file- code program:
+// └
+// B
