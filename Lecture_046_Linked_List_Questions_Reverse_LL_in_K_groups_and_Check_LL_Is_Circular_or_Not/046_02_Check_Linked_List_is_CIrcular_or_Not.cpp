77256b26ee7ddd14c95c018796bcec8947d90034
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-11-21 09:10:41 +0530

    046_02_Check_Linked_List_is_CIrcular_or_Not.cpp

---
+++ Lecture_046_Linked_List_Questions_Reverse_LL_in_K_groups_and_Check_LL_Is_Circular_or_Not/046_02_Check_Linked_List_is_CIrcular_or_Not.cpp
@@ -0,0 +1,128 @@
+
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    // constructor:
+    Node(int d){
+        this -> data = d;
+        this -> next = NULL;
+    }
+
+    ~Node(){
+        int value = this->data;
+        if(this->next != NULL){
+            delete next;
+            next = NULL;
+        }
+        cout << "memory is free for node with data" << value << endl;
+    }
+};
+
+
+void insertNode(Node* &tail, int element, int d){
+    // assuming that the element is present in the list
+    if (tail == NULL){
+        Node* newNode = new Node(d);
+        tail = newNode;
+        newNode -> next = newNode;
+}else{
+   // non-empty list
+   // assuming that the element is present in the list
+
+   Node* curr = tail;
+   while(curr -> data != element) {
+    curr = curr -> next;
+   }
+
+   // element foud -> curr is representing the node - element
+   Node* temp = new Node(d);
+   temp -> next = curr -> next;
+   curr -> next = temp;
+    }
+}
+
+void print(Node* tail){
+    Node* temp = tail;
+    
+    // empty list
+    if (tail == NULL){
+        cout << "List is empty: " << endl;
+        return;
+    }
+    do{
+        cout << tail -> data << " ";
+        tail = tail -> next;
+    }while(tail != temp);
+        cout << endl;
+}
+
+
+void deleteNode(Node* &tail, int value){
+    // empty list
+    if (tail == NULL){
+        cout << "List is empty, please check again! " << endl;
+        return;
+    }else{
+        // non - empty
+        // assuming that "value" is present in the linked list
+        Node* prev = tail;
+        Node* curr = prev -> next;
+
+        while(curr -> data != value){
+            prev = curr;
+            curr = curr -> next;
+        }
+
+        prev -> next = curr -> next;
+        // 1 Node Linked List
+        if(curr == prev){
+            tail = NULL;
+        }
+        // >= 2 Node Linked List
+        else if (tail == curr){
+            tail = prev;
+        }
+        curr -> next = NULL;
+        delete curr;
+    }
+}
+
+bool isCircularList(Node* head){
+    // empty list
+    if (head == NULL){
+        return true;
+    }
+    Node* temp = head -> next;
+    while (temp != NULL && temp != head){
+        temp = temp -> next;
+    }
+    if (temp == head){
+        return true;
+    }
+    return false;  
+}
+int main(){
+    Node* tail = NULL;
+    // inserting in the empty list:
+    insertNode(tail, 5, 3);
+    print(tail);
+    
+    insertNode(tail, 3, 5);
+    print(tail);
+
+if (isCircularList(tail)){
+    cout << "Linked List is  circular" << endl;
+}else{
+    cout << "Linked List is not Circluar" << endl;
+}
+}
+
+// Output:
+// 3 
+// 3 5
+// Linked List is  circular
