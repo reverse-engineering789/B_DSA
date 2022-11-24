deaa2da86b389ba75363b48d132b345aefb0b715
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-11-24 19:59:20 +0530

    050_01_Check_Palindrome_In_Linked_List.cpp

--- Lecture_050_Check_Palindrome_In_LInke_List/050_01_Check_Palindrome_In_Linked_List.cpp
+++ Lecture_050_Check_Palindrome_In_LInke_List/050_01_Check_Palindrome_In_Linked_List.cpp
@@ -22,30 +22,33 @@
 // is 1 2 3 4 , which is not a palindrome
 // and Hence, the output is 0.
 // Your Task:
-// The task is to complete the function isPalindrome() which takes head as reference as the only parameter and returns true or false if linked list is palindrome or not respectively.
+// The task is to complete the function isPalindrome() which 
+// takes head as reference as the only parameter and returns true 
+// or false if linked list is palindrome or not respectively.
 
 // Expected Time Complexity: O(N)
-// Expected Auxialliary Space Usage: O(1)  (ie, you should not use the recursive stack space as well)
+// Expected Auxialliary Space Usage: O(1)  (ie, you should not 
+// use the recursive stack space as well)
 
 // Constraints:
 // 1 <= N <= 105
 
 
 //{ Driver Code Starts
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <stack>
-using namespace std;
-/* Link list Node */
-struct Node {
-  int data;
-  struct Node *next;
-  Node(int x) {
-    data = x;
-    next = NULL;
-  }
-};
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <iostream>
+// #include <stack>
+// using namespace std;
+// /* Link list Node */
+// struct Node {
+//   int data;
+//   struct Node *next;
+//   Node(int x) {
+//     data = x;
+//     next = NULL;
+//   }
+// };
 
 
 
