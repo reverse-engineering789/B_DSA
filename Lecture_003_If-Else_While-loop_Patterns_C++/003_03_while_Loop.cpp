5db534d321e506824fd86a122bf5fca1cfa6fec3
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-21 07:58:05 +0530

    003_03_while_Loop.cpp

---
+++ Lecture_003_If-Else_While-loop_Patterns_C++/003_03_while_Loop.cpp
@@ -0,0 +1,125 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+
+// int n = 10;
+
+// int i = 1;
+
+// while (i <= n){
+//     cout << i << endl;
+//     i = i + 1;
+// }
+
+// // Output:
+// // 1
+// // 2
+// // 3
+// // 4
+// // 5
+// // 6
+// // 7
+// // 8
+// // 9
+// // 10
+
+
+// // ----------------------------------------------------
+
+
+// // cALCULATING THE SUM 
+
+
+// int n = 100; 
+
+// int i = 1; 
+// int  sum = 0; 
+
+// while (i <= n){
+//     sum = sum + i ;
+//     i = i + 1;
+// }
+
+
+// cout << "value of sum is: " << sum << endl;
+
+// // Output:
+// // value of sum is 5050
+
+
+
+
+
+// // -----------------------------------------------------
+
+
+
+
+// // PRIME NUMBER OR NOT 
+
+// int n = 14;
+
+// int i = 2; 
+
+// while ( i < n){
+//     if (n % i == 0) {
+//    cout << "not prime for " << i << endl;
+//     }else{
+//    cout << "prime for " << i << endl;
+//     }
+//    i = i + 1;
+// }
+
+// // Output:
+// // not prime for 2
+// // prime for 3
+// // prime for 4
+// // prime for 5
+// // prime for 6
+// // not prime for 7
+// // prime for 8
+// // prime for 9
+// // prime for 10
+// // prime for 11
+// // prime for 12
+// // prime for 13
+
+// // using for loop:
+
+// int n  = 14;
+
+// for (int i = 1; i <= n; i++){
+//     if (n % i == 0){
+//         cout << "not prime for " << i << endl;
+//     }else{
+//          cout << " Prime for " << i << endl;
+//     }
+// }
+
+
+// // Output:
+// // not prime for 1
+// // not prime for 2
+// // Prime for 3
+// // Prime for 4
+// // Prime for 5
+// // Prime for 6
+// // not prime for 7
+// // Prime for 8
+// // Prime for 9
+// // Prime for 10
+// // Prime for 11
+// // Prime for 12
+// // Prime for 13
+// // not prime for 14
+
+ return 0;
+}                                                                                
+
+
+
