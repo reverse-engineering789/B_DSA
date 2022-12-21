5c99b0c40a34d817063872b5bc475a386daa7b2d
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-21 07:56:34 +0530

    003_01_If_else.cpp

---
+++ Lecture_003_If-Else_While-loop_Patterns_C++/003_01_If_else.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // let a = 2
+    // if (a > 0){
+    //     console.log(`${a} negative number`)
+    // }else{
+    //     console.log(`${a} negative number`)
+    // }
+
+    // // Output:
+    // // 2 negative number
+
+    // //------------------------------------------------------
+
+    // let a = 1
+    // let b = 2
+
+    // if (a > b){
+    //     console.log("a is greater")
+    // }
+
+    // if (a < b){
+    //     console.log("b is greater")
+    // }
+
+    // // output:
+    // // b is greater
+
+    // //------------------------------------------------------------
+
+    // let a = 0
+
+    // if (a > 0){
+    //     console.log("a is +ve")
+    // }
+
+    // else{
+    // if (a < 0){
+    //     console.log("a is -ve")
+    // }else{
+    //     console.log('a is zero')
+    // }
+    // }
+
+    // // output:
+    // // a is zero
+
+    int a = 0;
+
+        if (a > 0){
+        cout << "a is positive" << endl;
+       }else{
+            if (a < 0){
+            cout << "a is negative" << endl;
+            }else{
+            cout << "a is zero" << endl;
+        }
+    }
+    return 0;
+};
+
+// Output:
+// a is zero
