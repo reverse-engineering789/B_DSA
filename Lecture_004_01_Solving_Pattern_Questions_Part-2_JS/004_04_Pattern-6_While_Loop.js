d36c5be10dc12bc906b4f3612a63e4fc03763492
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-21 08:04:30 +0530

    renamed

---
+++ Lecture_004_01_Solving_Pattern_Questions_Part-2_JS/004_04_Pattern-6_While_Loop.js
@@ -0,0 +1,35 @@
+const n = 5
+
+let row = 1
+while ( row <= n ){
+    
+    let col = 1 
+    while (col <= row){
+        console.log('*')
+        col = col + 1
+    }
+    console.log(' ')
+    row = row + 1
+}
+
+
+// Output:
+// *
+ 
+// *
+// *
+
+// *
+// *
+// *
+ 
+// *
+// *
+// *
+// *
+
+// *
+// *
+// *
+// *
+// *
