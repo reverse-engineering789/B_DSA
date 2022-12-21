bab0af952890fbaf6f220f30cafc94ca21325420
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-22 00:36:15 +0530

    004_02_Pattern-4_While_Loop.cpp

---
+++ Lecture_004_01_Solving_Pattern_Questions_Part-2_C++/004_02_Pattern-4_While_Loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+  for(int i = 1; i <= 5; i++){
+    for(int j = 5; j >= 1; j--){
+        cout << j;
+    }
+    cout << endl;
+  }
+}
+
+
+// Output:
+// 54321
+// 54321
+// 54321
+// 54321
+// 54321
