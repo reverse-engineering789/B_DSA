95f62cc321c66084ffb0678970aef97a11ebca73
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-22 00:36:37 +0530

    004_03_Pattern-5_While_Loop.cpp

---
+++ Lecture_004_01_Solving_Pattern_Questions_Part-2_C++/004_03_Pattern-5_While_Loop.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 4;
+    // int length = 16;
+  for(int i = 1; i <= n; i++){
+    for(int j = i; j <= n; j++){
+        cout << j;
+    }
+    cout << endl;
+  }
+}
+
+
+
+
+//  // PROBLEM: NOT ABLE TO PRINT THE PATTERN IN THE NEW LINE
+
+
+
+// let n = 4
+
+// let  i = 1
+
+// let count = 1
+
+// while (i<=n){
+//     let j = 1
+// while(j<=n){
+//     console.log(`${count} ${''}`)
+//     count = count + 1
+//     j = j + 1
+// }
+// console.log('\n')
+// i = i + 1
+// }
+
+// // Output:
+// // 1 
+// // 2 
+// // 3 
+// // 4
+
+
+// // 5
+// // 6
+// // 7
+// // 8
+
+
+// // 9
+// // 10
+// // 11
+// // 12
+
+
+// // 13
+// // 14
+// // 15
+// // 16
+
+
+// //----------------------------------------------------------------------
