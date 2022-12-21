1e8c2432436e397d3e81bd87e58dd545f8e37b7a
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-21 07:56:48 +0530

    003_02_else_if.cpp

---
+++ Lecture_003_If-Else_While-loop_Patterns_C++/003_02_else_if.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int a = 0;
+if (a > 0){
+    cout << "a is +ve" << endl;
+}
+else if (a < 0) {
+    cout << "a is +ve" << endl;
+}else{
+    cout << "a is zero" << endl;
+}
+
+ return 0;
+}
+
+// Output:
+// a is zero
