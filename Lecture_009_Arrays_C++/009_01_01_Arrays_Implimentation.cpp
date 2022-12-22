8c271e36bc1112ab16b3ac177e3aebcf39c9c042
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-22 16:34:33 +0530

    009_01_01_Arrays_Implimentation.cpp

---
+++ Lecture_009_Arrays_C++/009_01_01_Arrays_Implimentation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int arr[6] = {2,4,6,8,10,12};
+cout << arr[5] << endl;
+cout << arr[0] << endl;
+ return 0;
+} 
+
+// Output:
+// 12
+// 2
