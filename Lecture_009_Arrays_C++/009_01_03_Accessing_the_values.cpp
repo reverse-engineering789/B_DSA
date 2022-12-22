2e8d8b6777c7f2c3a7df4fd00876f6a759f48a3a
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-22 16:51:14 +0530

    009_01_03_Accessing_the_values.cpp

---
+++ Lecture_009_Arrays_C++/009_01_03_Accessing_the_values.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using  namespace std;
+
+int main(){
+
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+
+    // int arr[8] = 50;
+
+     cout << "value at 4th index " << arr[5] << endl;
+     cout << "value at 11th index " << arr[11] << endl;
+     cout << "value at 12th index " << arr[12] << endl;
+    
+// Output:
+// value at 4th index 6
+// value at 11th index 3682304
+// value at 12th index 6422352
+}
