8841bb72b28a3969bb2e25d97bdb982c9ef03d68
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-26 10:25:47 +0530

    023_06_Print_Like_A_Wave.cpp

---
+++ Lecture_023_Introduction_to_2D_Arrays_C++/023_06_Print_Like_A_Wave.cpp
@@ -0,0 +1,79 @@
+// Print Like a wave:
+
+// Problem statement:
+// For a given two-dimensonal integer arrray/list 'ARR' of size 
+// (N * M), print the 'ARR' in a sine wave order, i.e., print 
+// the first column top to bottom, next column to top, and so on.
+
+// For eg:-
+
+// The sine wave for the matrix:-
+// 1 2
+// 3 4
+// will be [1,3,4,2].
+
+
+// Detailed explanation ( Input/output format, Notes, Constraints, Images )
+// Sample Input 1:
+// 2
+// 3 4
+// 1 2 3 4
+// 5 6 7 8
+// 9 10 11 12
+// 4 4
+// 1 2 4 5
+// 3 6 8 10
+// 11 12 13 15
+// 16 14 9 7
+// Sample Output 1:
+// 1 5 9 10 6 2 3 7 11 12 8 4
+// 1 3 11 16 14 12 6 2 4 8 13 9 7 15 10 5 
+// Explanation For Sample Input 1:
+// Here, the elements are printed in a form of a wave, first, the 
+// 0th column is printed from top to bottom then the 1st column 
+// from bottom to top, and so on. Basically, the even column is 
+// printed from top to bottom and the odd column in the opposite 
+// direction.
+// Sample Input 2:
+// 2
+// 1 1
+// 3
+// 1 2
+// 6 5
+// Sample Output 2:
+// 3
+// 6 5 
+
+
+
+
+
+#include <bits/stdc++.h> 
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    //Write your code here
+    vector<int> ans;
+    
+    for(int col = 0; col < mCols; col++){
+        
+        if(col & 1){
+            // odd Index -> Bottom to top
+            
+            for(int row = nRows - 1; row >= 0; row--){
+//                 cout << arr[row][col] << " ";
+                   ans.push_back(arr[row][col]);
+            }
+        }else{
+            // 0 or ever index -> top to bottom
+            for(int row = 0; row < nRows; row++){
+//                 cout << arr[row][col] << " " ;
+                ans.push_back(arr[row][col]);
+            }
+        }
+    }
+    return ans;
+}
+
+
+
+
