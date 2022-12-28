b7944ec289f858edbec72a3f33a3842e98a44180
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-28 09:49:25 +0530

    switch implementation

---
+++ Lecture_008_Switch_Cases_and_functions_C++/008_01_01_Switch_Implementation.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num = 2;
+
+    cout << endl;
+    
+    switch(num){
+        case 1: cout << "Frist" << endl;
+                break;
+        
+        case 2: cout << "Second" << endl;
+                break;
+        
+        default: cout << "It is default case" << endl;
+    }
+    cout << endl;
+   return 0;
+}
+
+// Output:
+// Second
+
+
+// // --------------------------------------------------------
+
+
+
