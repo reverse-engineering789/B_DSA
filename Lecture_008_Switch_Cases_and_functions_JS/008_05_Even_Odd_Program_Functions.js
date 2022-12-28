3049a95eab16fb7525a57028e369b0b2081d7560
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-28 09:46:28 +0530

    renamed

---
+++ Lecture_008_Switch_Cases_and_functions_JS/008_05_Even_Odd_Program_Functions.js
@@ -0,0 +1,25 @@
+let isEven = (a) => {
+    if (a % 2){
+        return 0
+    }else{
+        return 1
+    }
+}
+
+let num = 4 
+
+if(isEven(num)){
+    console.log('number is even')
+}else{
+    console.log('number is odd')
+}
+
+// Output:
+// number is even
+
+
+
+//-----------------------------------------------------------------------
+
+
+
---
+++ Lecture_008_Switch_Cases_and_functions_JS/008_07_Counting_Program.js
@@ -0,0 +1,57 @@
+// const n = 10
+
+// let string = ''
+
+
+// for (let i = 1; i <= n; i++ ){
+//      string = string + i + ' '
+// }
+
+// console.log(string)
+
+// // Output:
+// // 1 2 3 4 5 6 7 8 9 10 
+
+// //----------------------------------------
+
+
+
+
+const n = 10
+
+let string = ''
+
+
+for (let i = 1; i <= n; i++ ){
+     string += i + ' '
+}
+
+console.log(string)
+
+// Output:
+// 1 2 3 4 5 6 7 8 9 10 
+
+//-------------------------------------------------------
+
+// // counting program: 
+
+// let printCounting = (n) => {
+//     for (let i = 1; i <= n; i++){
+//         console.log(i)
+//     }
+// }
+
+// let test = printCounting(10)
+
+// // Output:
+// // 1
+// // 2
+// // 3
+// // 4
+// // 5
+// // 6
+// // 7
+// // 8
+// // 9
+// // 10
+
