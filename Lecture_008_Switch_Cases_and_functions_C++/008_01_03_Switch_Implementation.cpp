688bdbbbb0bfb26d1c012451376d279f2dbed4fe
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-28 09:49:49 +0530

    008_01_03_Switch_Implementation.cpp

---
+++ Lecture_008_Switch_Cases_and_functions_C++/008_01_03_Switch_Implementation.cpp
@@ -0,0 +1,25 @@
+// default statement is not nessesory
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ch = 'l';
+
+    cout << endl;
+    
+    switch(ch){
+        case 1: cout << "Frist" << endl;
+                break;
+        
+        case 'l': cout << "Second" << endl;
+                break;
+        
+        // default: cout << "It is default case" << endl;
+    }
+    cout << endl;
+   return 0;
+}
+
+// Output:
+// second
