8131606341387cfab8a89aa708a7beda8c325fa1
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-29 13:09:24 +0530

    Lecture_008_Switch_Cases_and_functions_C++/008_05_02_Even_Odd_Program.cpp

---
+++ Lecture_008_Switch_Cases_and_functions_C++/008_05_02_Even_Odd_Program.cpp
@@ -0,0 +1,24 @@
+// odd or even using modulo operator, my own solution
+
+#include <iostream>
+using namespace std;
+
+// 1 -> Even
+// 0 -> odd
+
+
+bool isEven(int a){
+    if (a % 2 == 1){
+       return 0;
+    }else{
+        return 1;
+    }
+}
+int main(){
+    cout << "Enter the number: " << endl;
+    int a;
+    cin >> a;
+    int answer = isEven(a);
+    cout << "The number is: " << answer << endl;
+    return 0;
+}
