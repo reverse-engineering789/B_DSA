4bfaeb71abfaedd8cae02dfacc115a97b0714c52
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-30 10:30:46 +0530

    renamed-reformed in structure

---
+++ Lecture_021_LeetCode_CodeStudio_Questions_Arrays_JS/021_01_01_L_B_Roate_Array_Leetcode.js
@@ -0,0 +1,114 @@
+// this solution works on vs code but not accepted 
+var rotate = function(nums, k) {
+    console.log(nums)
+    let empty1 = []
+    let empty2 = []
+   for (let i = k+1; i < nums.length; i++){
+       empty1.push(nums[i])
+      
+   }
+   for (let j = 0; j <= k; j++){
+       empty2.push(nums[j]) 
+       
+   }
+      return empty1.concat(empty2);
+};
+nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+console.log(rotate(nums, k))
+
+// Problem Qustion:
+
+//LeetCode: Rotate Array
+
+// this code is little difficult, and approach is also little complex 
+// need to take MOD and all. its better to visit vid, before solving this 
+// problem. 
+
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+// Example 2:
+
+// Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+// Explanation: 
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -231 <= nums[i] <= 231 - 1
+// 0 <= k <= 105
+
+
+
+
+// rotating the array and Printing the rotated array:
+
+
+const rotate = (nums, k) => {
+    let temp = []
+    for (let  i = 0; i < nums.length; i++){
+        temp[(i + k)%nums.length] = nums[i]
+         
+    }
+      nums = temp
+      
+console.log(temp)
+
+}
+
+
+const Array = [1, 2, 3, 4, 5];
+const K = 3;
+
+rotate(Array, K)
+
+// Output:
+// [ 3, 4, 5, 1, 2 ]
+
+
+
+
+// ----------------------------------------------------------
+
+
+// printing the elements in the rotated array one by one:
+
+const rotate1 = (nums, k) => {
+    let temp = []
+    for (let  i = 0; i < nums.length; i++){
+        temp[(i + k)%nums.length] = nums[i]  
+    }
+      nums = temp
+
+      for (let i = 0; i < temp.length; i++){
+          console.log(temp[i])
+      }
+}
+
+
+const Array1 = [1, 2, 3, 4, 5];
+const K1 = 3;
+
+rotate1(Array1, K1)
+
+
+// Output:
+// 3
+// 4
+// 5
+// 1
+// 2
