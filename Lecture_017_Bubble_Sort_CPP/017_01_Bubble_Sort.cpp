082927708183573a52805d5ea89152b1779bd2f2
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-12-30 10:45:15 +0530

    bubble sort

--- Lecture_017_Bubble_Sort_CPP/017_01_Bubble_Sort.cpp
+++ Lecture_017_Bubble_Sort_CPP/017_01_Bubble_Sort.cpp
@@ -27,17 +27,17 @@
 // Sample Output 2:
 // 1 2
 // 1 2 3 4
-// #include <bits/stdc++.h> 
-// void bubbleSort(vector<int>& arr, int n)
-// {   
-//     // Write your code here.
-//     for(int i = 1; i < n; i++){
-//         // for round 1 to n-1
+#include <bits/stdc++.h> 
+void bubbleSort(vector<int>& arr, int n)
+{   
+    // Write your code here.
+    for(int i = 1; i < n; i++){
+        // for round 1 to n-1
         
-//         for(int j = 0; j < n-i; j++){
-//             if(arr[j] > arr[j+1]){
-//                 swap(arr[j], arr[j+1]);
-//             }
-//         }
-//     }
-// }
+        for(int j = 0; j < n-i; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
